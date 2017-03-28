fb0b755da29c52a962d3c56dfb6a9465cfe8a892
caoh1 <hailengc@gmail.com>
2017-03-28 21:42:36 +0800

    add the first test using minitest

--- test/test_binaryheap.rb
+++ test/test_binaryheap.rb
@@ -1,1 +1,11 @@
 # unit test for binaryheap
+# magic_ball.rb
+require 'minitest/autorun'
+require 'binaryheap'
+
+class BinaryHeapTest < Minitest::Test
+  def test_init_with_no_parameter
+    bh = BinaryHeap.new
+    assert bh.data != nil
+  end  
+end
